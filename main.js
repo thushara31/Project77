e7acc4f275c21bd46ba4414c98b4baa2aa1a96fc
thushara31 <128104176+thushara31@users.noreply.github.com>
2023-09-21 20:09:00 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name = "Thushara";
+var last_name = "Pogula";
+
+var name= first_name +" "+ last_name;
+console.log(name);
+var name1= first_name.concat(last_name);
+console.log(name1);
+
+var a =31;
+var b = 21;
+var c= a+b;
+console.log(c);
+
